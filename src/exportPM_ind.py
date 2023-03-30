1060b7684e192ac3705def82eed0b65d62bc0c19
John LaMaster <7785925+JohnLaMaster@users.noreply.github.com>
2023-03-30 10:54:27 +0200

    Update exportPM_ind.py
    Finalized

--- src/exportPM_ind.py
+++ src/exportPM_ind.py
@@ -3,23 +3,30 @@ import json
 import os
 import sys
 
-import scipy.io as io
-from aux import _save, prepare
+from main_fcns import _save, prepare
 
 sys.path.append('../')
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--savedir', type=str, help='Where to save the PM index')
-    parser.add_argument('--config_file', type=str, default='./src/configurations/debug_new_init.json', help='Config file path for defining the PM')
+    parser.add_argument('--savedir', type=str)
+    parser.add_argument('--name', type=str, default=None, help='Specify custom file name for saved index without a file extension')
+    parser.add_argument('--config_file', type=str, default='./src/configurations/debug_new_init.json')#DL_PRESS_144_ge.json')
 
     args = parser.parse_args()
 
+    # Create the save directory and construct the file name
     os.makedirs(args.savedir, exist_ok=True)
+    if isinstance(args.name, type(None)):
+        path = os.path.join(args.savedir,'mrs-sim_PM_ind.json')
+    else:
+        name, _ = os.path.splitext(args.name)
+        path = os.path.join(args.savedir, name + '.json')
 
-    # Simulate
+    # Define the index
     _, _, _, _, _, ind, _, _ = prepare(args.config_file)
-   
-    path = os.path.join(args.savedir, 'physics_model_index.mat')
-    io.savemat(path, do_compression=True, mdict={"ind": ind})
+
+    # Export the PM index
+    with open(path, 'w+') as file:
+        json.dump(ind, file, indent=4, separators=(',', ': '))
     print('Saved physics model index at: {}'.format(path))
