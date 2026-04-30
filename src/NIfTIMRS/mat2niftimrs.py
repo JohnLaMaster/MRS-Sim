8d1e86dc1e144f120636ad576aac6a9013961033
John LaMaster <jlamaste@gmail.com>
2026-04-30 15:39:06 +0200

    Debugged nifti conversion but still need to finish upgrades

--- src/NIfTIMRS/mat2niftimrs.py
+++ src/NIfTIMRS/mat2niftimrs.py
@@ -10,6 +10,8 @@ import numpy as np
 import scipy.io as io
 from pathlib import Path
 
+from src.aux import loadmat_as_dict
+
 # from torch.nn import Module
 
 __all__ = ['Mat2NIfTI_MRS']
@@ -29,39 +31,53 @@ class Mat2NIfTI_MRS():
             self.currNiftiOrientation = pickle.load(file)
 
         self.combine_complex = combine_complex
-        self.test_output = test_output
+        self.test = test_output
 
 
     def forward(self, 
                 datapath: str,
-                reshape: list=None
+                reshape: list=None,
+                label: str=None
                ):
 
         # Load the simualted data
-        with open(datapath, 'r') as file:
-            data = io.loadmat(file)
+        with open(datapath, 'rb') as file:
+            # print('datapath: ',datapath)
+            data = loadmat_as_dict(file)
             specDataCmplx = data['spectra']
             header = data['header']
 
+        if isinstance(label,type(None)) and specDataCmplx.shape[1]>>1:
+            specDataCmplx = np.expand_dims(specDataCmplx[:,0,...], axis=1)
+        elif isinstance(label,str) and specDataCmplx.shape[1]>=2:
+            if "noise_free" in label.lower():
+                specDataCmplx = np.expand_dims(specDataCmplx[:,1,...], axis=1)
+            elif "filtered" in label.lower():
+                specDataCmplx = np.expand_dims(specDataCmplx[:,2,...], axis=1)
+                
         # Combine real and imaginary to store as complex-valued
         if self.combine_complex:
+            # print("specDataCmplx.shape before: ",specDataCmplx.shape)
             specDataCmplx = specDataCmplx[...,0,:] + 1j*specDataCmplx[...,1,:]
-            specDataCmplx = np.squeeze(specDataCmplx,axis=-2)
+            # print("specDataCmplx.shape after: ",specDataCmplx.shape)
+            # specDataCmplx = np.squeeze(specDataCmplx,axis=-2)
 
         # Reshape data to fit into the container
         x, y = reshape if not isinstance(reshape, type(None)) else 1, 1
-        z = specDataCmplx.shape[0] / (x*y)
-        s = specDataCmplx.shape[-1]
+        z = int(specDataCmplx.shape[0] / (x*y))
+        s = int(specDataCmplx.shape[-1])
         specDataCmplx = specDataCmplx.reshape(x,y,z,s)
 
         path, name = os.path.split(datapath)
+        save_name = os.path.splitext(name)[0]
+        if label: save_name = os.path.join(save_name, label)
 
         # Define the metadata
         json_full, meta_dict = self.set_metadata(name, header)
 
         # Write the NIfTI files
         self.write_NIfTIMRS(specDataCmplx, self.currNiftiOrientation, header, 
-                            json_full, path, os.path.splitext(name)[0])
+                            json_full, path, save_name, meta_dict)
 
 
     def write_NIfTIMRS(self, 
@@ -71,13 +87,14 @@ class Mat2NIfTI_MRS():
                        json_full,
                        save_path,
                        save_name,
+                       meta_dict
                        ):
         # Write the NIfTI-MRS file
         newobj = nib.nifti2.Nifti2Image(specDataCmplx, currNiftiOrientation.Q44)
 
         # Write new header
         pixDim = newobj.header['pixdim']
-        pixDim[4] = header.dwelltime
+        pixDim[4] = header['dwelltime']
         newobj.header['pixdim'] = pixDim
 
         # Set q_form >0
@@ -91,7 +108,7 @@ class Mat2NIfTI_MRS():
         newobj.header.extensions.append(extension)
 
         # # From nii obj and write    
-        nib.save(newobj, save_path / save_name + '.nii.gz')
+        nib.save(newobj, os.path.join(save_path, save_name + '.nii.gz'))
 
         if self.test:
             # Check the integrity of the stored data and header information
@@ -99,10 +116,10 @@ class Mat2NIfTI_MRS():
 
 
     def set_metadata(self, path: str, header):
-        spectrometer_frequency_hz = header.carrier_frequency
+        spectrometer_frequency_hz = header['carrier_frequency']
         nucleus_str = '1H'
 
-        echo_time_s = header.TE / 1000
+        echo_time_s = header['TE'] / 1000
         repetition_time_s = 'NA'
 
         DeviceSerialNumber = 'NA'
@@ -145,15 +162,16 @@ class Mat2NIfTI_MRS():
 
     def test_output(self, save_path, save_name, specDataCmplx, json_full, meta_dict):
         # Write hdf5 to check against using original information
-        with h5py.File(save_path / save_name +'.h5', 'w') as h5f:
+        base_name = os.path.join(save_path, save_name)
+        with h5py.File(base_name +'.h5', 'w') as h5f:
             h5f.create_dataset(save_name+'_data', data=specDataCmplx)
             h5f.create_dataset(save_name+'_header_ext', data=json_full)
 
         # Load NIfTI file
-        check_nifti = nib.load(save_path / save_name + '.nii.gz')
+        check_nifti = nib.load(base_name + '.nii.gz')
 
         # Load h5py file
-        with h5py.File(save_path / save_name + '.h5','r') as h5f:
+        with h5py.File(base_name + '.h5','r') as h5f:
             check_hdf5 = h5f[save_name+'_data'][:]
             
         # Compare header and data from NIfTI and h5py
@@ -165,5 +183,5 @@ class Mat2NIfTI_MRS():
         assert loaded_he_content == meta_dict
 
         # Delete testing data
-        os.remove(save_path / save_name +'.h5')
+        os.remove(base_name +'.h5')
         del loaded_he_content
